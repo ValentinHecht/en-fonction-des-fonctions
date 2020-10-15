2ad1df5f586ec02f49961a5536b4a449a309cbb0
Benjamin <benjy.chane@gmail.com>
2020-10-15 16:10:40 +0200

    nouvelle branche

--- src/jeton.h
+++ src/jeton.h
@@ -5,33 +5,41 @@
 
 * FISA-TI23 Copyright 2020-2020
 ********************************************************************************************/
+
+
 // Énumération des différents types de lexems existants
 typedef enum{
-    REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
-}typelexem;
+    REEL, OPERATEUR, FONCTION, ERREUR, FIN, PAR_OUV, PAR_FERM, VARIABLE, BAR_OUV, BAR_FERM, ABSOLU
+} typelexem;
+
 // Énumération des différents types d'opérateur existants
 typedef enum{
-    PLUS,MOINS,FOIS,DIV,PUIS
-}typeoperateur;
+    PLUS, MOINS, FOIS, DIV, PUIS
+} typeoperateur;
+
 // Énumération des différents types de fonction existantes
 typedef enum{
     ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
-}typefonction;
+} typefonction;
+
 // Énumération des différentes erreurs possible (coté évaluateur)
 typedef enum{
     DIV_ZERO
-}typeerreur;
+} typeerreur;
+
 // Énumération des différents types de valeurs existantes
 typedef union{
     float reel;
     typefonction fonction;
     typeoperateur operateur;
     typeerreur erreur;
-}typejeton;
+} typejeton;
+
 // Énumération des différents types de jetons existants
 typedef struct Node{
     typejeton jeton;
     struct Node *pjetonpreced;
     struct Node *pjetonsuiv;
-}Node;
+} Node;
+
 typedef Node *Arbre;
--- src/syntaxe.c
+++ src/syntaxe.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include "jeton.h"
 
-int main() {
-    printf("Hello");
 
-    return 0;
+Node *syntaxe (typejeton tabJeton[], int position) {
+
+    // sin(x+2)
+    // sin(x++3))
+    // doiqsnd(x+3)
+
+    // fonction, variable, opérande, réel
+
+    typejeton jeton;
+
+    Node *arbre = NULL;
+
+    int i = 0;
+    size_t length = sizeof(tabJeton);
+
+    while (i != length) {
+        switch (jeton.erreur)
+        {
+        case tabJeton[i].jeton :
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+    return arbre;
 }
