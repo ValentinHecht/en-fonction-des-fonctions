f1b01b6a6b3f09f9379948e09a17c3c824d03111
unknown <killianb59@gmail.com>
2020-10-15 16:14:29 +0200

    modified:   src/jeton.h 	modified:   src/lexical.c 	modified:   src/lexical.h

--- src/jeton.h
+++ src/jeton.h
@@ -8,30 +8,36 @@
 // Énumération des différents types de lexems existants
 typedef enum{
     REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
-}typelexem;
+} typelexem;
+
 // Énumération des différents types d'opérateur existants
 typedef enum{
     PLUS,MOINS,FOIS,DIV,PUIS
-}typeoperateur;
+} typeoperateur;
+
 // Énumération des différents types de fonction existantes
 typedef enum{
     ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
-}typefonction;
+} typefonction;
+
 // Énumération des différentes erreurs possible (coté évaluateur)
 typedef enum{
-    DIV_ZERO
-}typeerreur;
+    DIV_ZERO, SQRT_NEG, PAR_NONFERM, PAR_NONOUVER, DOUBLE_OPE, ORTO_FONC, MAJ_FONC
+} typeerreur;
+
 // Énumération des différents types de valeurs existantes
 typedef union{
     float reel;
     typefonction fonction;
     typeoperateur operateur;
     typeerreur erreur;
-}typejeton;
+} typejeton;
+
 // Énumération des différents types de jetons existants
 typedef struct Node{
     typejeton jeton;
     struct Node *pjetonpreced;
     struct Node *pjetonsuiv;
-}Node;
+} Node;
+
 typedef Node *Arbre;
--- src/lexical.c
+++ src/lexical.c
@@ -7,12 +7,38 @@
 ********************************************************************************************/
 
 #include <stdio.h>
-//#include "jeton.h"
+#include <ctype.h>
+#include "jeton.h"
 #include "lexical.h"
 
-char* analyse_lexical(char* op)  {
-    printf("Vous avez ecrit : %s", op);
+int i = 0;
 
+char* analyse_lexical(char* op)  {
+    //printf("Vous avez ecrit : ");
+
+    while(op[i] != '\0') {
+        //printf("%c", op[i]);
+
+        if ((op[i] == 's' && op[i+1] == 'i' && op[i+2] == 'n') || (op[i] == 'S' && op[i+1] == 'I' && op[i+2] == 'N')) {
+            puts("Fonction SIN detecte !");
+            i = i+3;
+        } else if ((op[i] == 's' && op[i+1] == 'q' && op[i+2] == 'r' && op[i+3] == 't') || (op[i] == 'S' && op[i+1] == 'Q' && op[i+2] == 'R' && op[i+3] == 'T')) {
+            puts("Fonction SQRT detecte !");
+            i = i+4;
+        } else if (op[i] == '(') {
+            puts("Parenthese ouvrante detecte !");
+            i++;
+        } else if (op[i] == ')') {
+            puts("Parenthese fermante detecte !");
+            i++;
+        } else if (isdigit(op[i])) {
+            puts("Chiffre detecte !");
+            i++;
+        } else {
+            i++;
+        }
+
+    }
 
     return op;
 };
