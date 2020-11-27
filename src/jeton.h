b057fe31552af50bc61cf99a5082d01704f02a82
Lucas Bremard <lbremard62@gmail.com>
2020-11-27 18:57:10 +0100

    branch prepared to merge

--- src/jeton.h
+++ src/jeton.h
@@ -8,23 +8,25 @@
 // Énumération des différents types de lexems existants
 
 typedef enum{
-    REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
-}typelexem;
+    REEL = 0, OPERATEUR = 1, FONCTION = 2, ABSOLU = 3, 
+    ERREUR = 4, FIN = 5, PAR_OUV = 6, PAR_FERM = 7, 
+    VARIABLE = 8, BARRE = 9
+} typelexem;
 
 // Énumération des différents types d'opérateur existants
 typedef enum{
-    PLUS,MOINS,FOIS,DIV,PUIS
-}typeoperateur;
+    PLUS = 10, MOINS = 11, FOIS = 12, DIV = 13, PUIS = 14
+} typeoperateur;
 
 // Énumération des différents types de fonction existantes
 typedef enum{
-    ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
-}typefonction;
+    ABS = 15, SIN = 16, SQRT = 17, LOG = 18, COS = 19, 
+    TAN = 20, EXP = 21, ENTIER = 22, VAL_NEG = 23, SINC = 24
+} typefonction;
 
 // Énumération des différentes erreurs possible (coté évaluateur)
 typedef enum{
-    DIV_ZERO, DOUBLE_OPE, SQRT_NEG, PAR_NONFERM, PAR_NONOUVER, ORTO_FONC, MAJ_FONC, BAR_NONFERME
-
+    DIV_ZERO, DOUBLE_OPE, SQRT_NEG, PAR_NONFERM, PAR_NONOUVER, ORTO_FONC, MAJ_FONC, BAR_NONFERME, OPE_PAR, BAR_ABS
 }typeerreur;
 
 // Énumération des différents types de valeurs existantes
@@ -35,18 +37,29 @@ typedef union{
     typeerreur erreur;
 }typevaleur;
 
-//Structure lexem
-typedef struct Jeton {
+// Énumération des différents types de jetons existants
+typedef struct{
     typelexem lexem;
     typevaleur valeur;
-}typejeton;
+} typejeton;
 
-// Énumération des différents types de jetons existants
+//Structure lexem Node
 typedef struct Node{
     typejeton jeton;
     struct Node *pjetonpreced;
     struct Node *pjetonsuiv;
-}Node;
+    struct Node *pjetonparent;
+    int colonne;
+    int couche;
+} Node;
+
+//Structure lexem
+typedef struct Jeton {
+    typelexem lexem;
+    float reel;
+    typefonction fonction;
+    typeoperateur operateur;
+    typeerreur erreur;
+} Jeton;
+
 typedef Node *Arbre;
-
-
