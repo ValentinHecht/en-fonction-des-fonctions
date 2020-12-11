9a63cf57d8ab452df5c44544f2c735e3bcf7f897
Benjamin <benjy.chane@gmail.com>
2020-12-11 13:34:53 +0100

    finish

--- src/main.c
+++ src/main.c
@@ -131,9 +131,6 @@ int main() {
     test109[12].lexem = PAR_FERM;
 
     
-    
-    
-    
     // div zero : cos(x)/0;
     Jeton test_div_zero[6]; // TEST OK
     test_div_zero[0].lexem = FONCTION;
@@ -169,28 +166,27 @@ int main() {
 
     // ERREUR 107 
     // Fontion : '|-5';???
-    Jeton erreur107[4]; //TEST OK
+    Jeton erreur107[3]; //TEST OK
 
     erreur107[0].lexem = BARRE;
     erreur107[1].lexem = OPERATEUR;
     erreur107[1].operateur = MOINS;
     erreur107[2].lexem = REEL;
     erreur107[2].reel = 5;
-    erreur107[3].lexem = BARRE;
+    
     
 
     // 4
 
     // test erreurs 
-    
-    size_t length = sizeof(erreur107)/sizeof(erreur107[0]);
+    size_t length = sizeof(expression)/sizeof(expression[0]);
 
     //printf("%d", length);
 
     printf("\n\n");
     Node *arbre;
     //arbre  = create_tree(expression, length);
-    arbre  = syntaxe(erreur107, length);
+    arbre  = syntaxe(expression, length);
 
 
     //syntaxe(graph, length);
--- src/newsyntaxe.h
+++ src/newsyntaxe.h
@@ -1,6 +1,82 @@
 #include "jeton.h"
 
-int *fourchette_operateur(int debut[], int fin[], int length, int pos);
+
+
+/**
+ * Fonction regroupant les tests d'erreurs et la création de l'arbre
+ * Destiné à être utiliser par la partie évaluateur
+ * 
+ * @param tabJeton[] : tableau de jeton que LEXICAL NOUS DONNE
+ * @param elem : nombre d'elements du tableau de jeton que LEXICAL NOUS DONNE
+ * 
+ **/
+Node *syntaxe(Jeton tabJeton[], int elem);
+
+
+/**
+ * Fonction de détection d'erreurs
+ * 
+ * @param tabJeton[] : tableau de jeton que LEXICAL NOUS DONNE
+ * @param elem : nombre d'elements du tableau de jeton que LEXICAL NOUS DONNE
+ * 
+ **/
+void errors_detection(Jeton tabJeton[], int elem);
+
+
+/**
+ * Fonction principale de la création de l'arbre
+ * 
+ * @param expression : tableau de jeton que LEXICAL NOUS DONNE
+ * @param elem : nombre d'elements du tableau de jeton que LEXICAL NOUS DONNE
+ * 
+ * @return : retourne l'arbre créé
+ * 
+ **/
+Node *create_tree_recursive(Jeton *expression, int elem);
+
+
+/**
+ * Traitements des jetons
+ * 
+ * @param parent : c'est le parent
+ * @param expression : tableau de jeton à traiter
+ * @param elem : nombre d'éléments de "expression"
+ * @param num : bifurcation. si 0, on descend vers la gauche sinon vers la droite
+ * 
+ * @return : le noeud actuel avec plus de detail (colonne, couche, etc..)
+ **/
+Node *aller_a(Node *parent, Jeton *expression, int elem, int num);
+
+
+/**
+ * Creation d'un noeud
+ * 
+ * @param lexical[] : tableau de jetons
+ * @param elem : nombre d'éléments dans tabJeton
+ * @param position : prend la position du jeton correspondant au noeud actuel
+ * 
+ * @return : Noeud actuel
+ * 
+ **/
+Node *create_node(Jeton lexical[], int elem, int *position);
+
+
+/**
+ * Initialise les valeurs "colonne" et "couche" dans le node et le lie au parent
+ * 
+ * @param parent : c'est le parent du noeud
+ * @param node : noeud à traiter
+ * @param num : bifurcation. si 0, on descend vers la gauche sinon vers la droite
+ **/
+void init_value(Node *parent, Node *node, int num);
+
+
+
+
+
+
+
+
 
 /**
  * Cherche l'operande le moins prioritaire avec une expression comportant des parentheses
@@ -15,46 +91,12 @@ int *fourchette_operateur(int debut[], int fin[], int length, int pos);
 int operande_in_parentesis(int debut[], int fin[], int length, int pos);
 
 /**
- * Creation d'un noeud
+ * Cherche l'operande le moins prioritaire avec une expression comportant des parentheses
  * 
- * @param lexical[] : tableau de jetons
- * @param elem : nombre d'éléments dans tabJeton
- * @param position : prend la position du jeton correspondant au noeud actuel
+ * @param tab: tableau comprenant les jetons
+ * @param jeton: correspond à chaque jeton
+ * @param elem: nombre d'elements du tableau de jeton
  * 
- * @return : Noeud actuel
- * 
+ * @return: un integer
  **/
-Node *create_node(Jeton lexical[], int elem, int *position);
-
-/**
- * Fonction principale de la création de l'arbre
- * 
- * @param expression : tableau de jeton que LEXICAL NOUS DONNE
- * @param elem : nombre d'elements du tableau de jeton que LEXICAL NOUS DONNE
- * 
- * @return : retourne l'arbre créé
- * 
- **/
-Node *create_tree_recursive(Jeton *expression, int elem);
-
-/**
- * Traitements des jetons
- * 
- * @param parent : c'est le parent
- * @param expression : tableau de jeton à traiter
- * @param elem : nombre d'éléments de "expression"
- * @param num : bifurcation. si 0, on descend vers la gauche sinon vers la droite
- * 
- * @return : le noeud actuel avec plus de detail (colonne, couche, etc..)
- **/
-Node *aller_a(Node *parent, Jeton *expression, int elem, int num);
-
 int contains(Jeton tab[], typejeton jeton, int elem);
-
-void create_tree(Jeton tabJeton[]);
-
-int is_inPar(int debut[], int fin[], int pos);
-
-void test_erreur(Node *arbre, Jeton tab[]);
-
-Node *syntaxe(Jeton tabJeton[], int elem);
